8c4bcab417b87706b0a08c1d9fa5ca2179d61112
[Eidenas] <[eidenaspunys@gmail.com]>
2025-07-21 11:27:46 +0300

    Add responsive behavior to the nav

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -3,28 +3,31 @@
 import moon from "../../../src/assets/moon.svg";
 const Nav = () => {
   return (
-    <nav className="w-full flex-row flex h-[5rem] fixed shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-20">
-      <div className="flex w-3/4">
-        <h2 className="font-['Nunito_Sans',sans-serif] font-extrabold text-[1.5rem]">
+    <nav className="w-full flex-row flex h-[8rem] bg-white sticky shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-8 max-md:px-4 max-lg:px-12 top-0" >
+      <div className="flex w-3/4  max-2xl:w-1/2">
+        <h2 className="font-['Nunito_Sans',sans-serif] font-extrabold text-[2.4rem] max-md:text-[1.4rem]  ">
           Ekskursijos
         </h2>
       </div>
-      <div className="flex  w-1/4 justify-between">
-        <div className="flex gap-2">
-          <img src={moon} alt="Sign up icon"/>
-          <button className="font-['Nunito_Sans',sans-serif] font-semibold text-[1rem] cursor-pointer">
+      <div className="flex w-1/4 max-2xl:w-1/2 justify-between ">
+        <div className="flex gap-2 w-1/3 ">
+          <img src={moon} alt="Sign up icon" className="max-md:hidden" />
+          <button className="font-['Nunito_Sans',sans-serif] font-semibold text-[1.6rem] max-md:text-[1.2rem] cursor-pointer">
             Sign up
           </button>
         </div>
-        <div className="flex gap-2">
-          <img src={moon} alt="Log in icon"/>
-          <button className="font-['Nunito_Sans',sans-serif] font-semibold text-[1rem] cursor-pointer">
+        <div className="flex gap-2 w-1/3">
+          <img src={moon} alt="Log in icon" className="max-md:hidden" />
+          <button className="font-['Nunito_Sans',sans-serif] font-semibold text-[1.6rem] max-md:text-[1.2rem] cursor-pointer">
             Log in
           </button>
         </div>
-        <div className="flex gap-2">
-          <img src={moon} alt="Dark mode icon"/>
-          <button className="font-['Nunito_Sans',sans-serif] font-semibold text-[1rem] cursor-pointer">
+        <div
+          className="flex gap-2 w-1/3
+        "
+        >
+          <img src={moon} alt="Dark mode icon" className="max-md:hidden" />
+          <button className="font-['Nunito_Sans',sans-serif] font-semibold text-[1.6rem] max-md:text-[1.2rem] cursor-pointer">
             Dark Mode
           </button>
         </div>
