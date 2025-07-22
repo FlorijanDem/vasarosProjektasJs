b4eb075438ecf3763a2d61d475d06ec953d670a7
[Eidenas] <[eidenaspunys@gmail.com]>
2025-07-22 13:52:22 +0300

    Create login modal window and init Axios compability for Log in

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -2,9 +2,12 @@
 // you can use with media queries
 import { Link } from "react-router";
 import moon from "../../../src/assets/moon.svg";
+import { useState } from "react";
+import LoginModal from "../../pages/Login/LoginModal";
 const Nav = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <nav className="w-full flex-row flex h-[8rem] bg-white sticky shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-8 max-md:px-4 max-lg:px-12 top-0" >
+    <nav className="w-full flex-row flex h-[8rem] bg-white sticky shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-8 max-md:px-4 max-lg:px-12 top-0">
       <div className="flex w-3/4  max-2xl:w-1/2">
         <h2 className="font-['Nunito_Sans',sans-serif] font-extrabold text-[2.4rem] max-md:text-[1.4rem]  ">
           Ekskursijos
@@ -21,11 +24,18 @@ const Nav = () => {
         </div>
         <div className="flex gap-2 w-1/3">
           <img src={moon} alt="Log in icon" className="max-md:hidden" />
-          <Link>
-            <button className="font-['Nunito_Sans',sans-serif] font-semibold text-[1.6rem] max-md:text-[1.2rem] cursor-pointer">
-              Log in
-            </button>
-          </Link>
+          <button
+            onClick={() => {
+              setIsModalOpen(true);
+            }}
+            className="font-['Nunito_Sans',sans-serif] font-semibold text-[1.6rem] max-md:text-[1.2rem] cursor-pointer"
+          >
+            Log in
+          </button>
+             <LoginModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
         </div>
         <div
           className="flex gap-2 w-1/3
---
+++ frontend/src/pages/Login/LoginModal.jsx
@@ -0,0 +1,94 @@
+// "axios" using for make API queries
+// if you do not want to use it
+// you can use "fetch API"
+//
+// import axios from "axios";
+
+// "react-hook-form" using for make easier
+// make the same functionallity with JavaScript and HTML
+// also posible
+
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+import axios from 'axios';
+import { useState } from "react";
+
+const LoginModal = ({ isOpen, onClose }) => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  if (!isOpen) return null;
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post('http://localhost:3001/login', {
+        email: data.email,
+        password: data.password,
+      });
+      console.log(response.data)
+    } catch (error) {
+      console.error('Login failed:', error.response?.data || error.message);
+      setErrorMessage(error.response?.data?.message || 'Login failed');
+    }
+  };
+  return (
+    <div className="fixed inset-0 z-10 flex items-center justify-center max-lg:p-4 bg- bg-[#D9D9D9]/[var(--bg-opacity)] [--bg-opacity:40%] backdrop-blur-xs">
+      <div className="rounded-[1.25rem] bg-white relative w-1/3 h-2/3 flex flex-col items-center max-lg:w-full shadow-2xl  ">
+        <button onClick={onClose} className="text-[3rem] self-end px-8">
+          x
+        </button>
+        <h2 className="text-3xl font-semibold text-center">Log in</h2>
+        <div className="flex w-full h-4/5 justify-center items-center">
+          <form
+            className="flex flex-col h-1/2 w-full justify-center items-center gap-12"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+             <p className="text-3xl text-red-400">{errorMessage}</p>
+            <input
+              className="border-2 rounded-2xl h-4/16 w-6/10 p-8 text-[2rem] max-lg:w-8/10 "
+              type="email"
+              {...register("email", { required: true })}
+              placeholder="Email"
+            />
+            {errors.email && (
+              <p className="text-red-700 font-semibold text-2xl">
+                Email is required
+              </p>
+            )}
+            <input
+              className="border-2 rounded-2xl p-8 h-4/16 w-6/10 text-[2rem] max-lg:w-8/10"
+              type="password"
+              {...register("password", { required: true })}
+              placeholder="Password"
+            />
+            {errors.password && (
+              <p className="text-red-700 font-semibold text-2xl">
+                Password is required
+              </p>
+            )}
+
+            <Link>
+              <p className="text-[1.25rem]">Forgot password</p>
+            </Link>
+            <div className="flex gap-2 text-[1.25rem]">
+              <p>Don't have an account?</p>
+              <Link>
+                <p className="text-gray-900">Register here</p>
+              </Link>
+            </div>
+
+            <input
+              type="submit"
+              value="Log in"
+              className=" bg-white rounded-2xl border-2 h-2/16 w-2/16 text-[1.75rem]"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
