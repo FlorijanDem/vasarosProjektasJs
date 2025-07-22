87e5eb68fe9075b6f7a1b422c186a84f549651b0
Denisas <denisas.podub@gmail.com>
2025-07-22 12:54:10 +0300

    Add Initialize Missing Tables

--- backend/utils/postgres.js
+++ backend/utils/postgres.js
@@ -1,22 +1,25 @@
-const postgres = require("postgres");
-require("dotenv").config();
+require('dotenv').config();
+
+const postgres = require('postgres');
 
 const sql = postgres({
-  host: process.env.DB_HOST || "localhost", // change as needed
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "your_database",
-  username: process.env.DB_USER || "your_username",
-  password: process.env.DB_PASSWORD || "your_password",
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  database: process.env.DB_NAME || 'your_database',
+  username: process.env.DB_USER || 'your_username',  
+  password: process.env.DB_PASSWORD || 'your_password',
 });
 
 const testConnection = async () => {
   try {
-    await sql`SELECT 1 AS result`;
-    console.log("✅ Connection to database successful");
+    await sql`SELECT 1`;
+    console.log('DB connection successful');
   } catch (error) {
-    console.error("❌ Connection to database failed:", error);
-    throw error; // Re-throw the error to handle it in the server file
+    console.error('DB connection failed:', error);
   }
 };
 
-module.exports = { sql, testConnection };
+module.exports = {
+  sql,
+  testConnection,
+};
