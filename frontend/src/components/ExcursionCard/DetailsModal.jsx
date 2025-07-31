20f556d6215543654b4c91ee9d751c713ccc4c18
ingrida <ingridakvetkovskyte@gmail.com>
2025-07-31 15:53:11 +0300

    Add details modal content

--- frontend/src/components/ExcursionCard/DetailsModal.jsx
+++ frontend/src/components/ExcursionCard/DetailsModal.jsx
@@ -1,5 +1,7 @@
 import styles from "./detailsModal.module.css";
+import "react-day-picker/dist/style.css";
 import { useRef, useEffect } from "react";
+import { DayPicker } from "react-day-picker";
 
 const DetailsModal = ({ excursion, onClose }) => {
   const modalRef = useRef(null);
@@ -21,24 +23,34 @@ const DetailsModal = ({ excursion, onClose }) => {
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, [onClose]);
 
+  const availableDates = excursion.tour_dates.map((date) => new Date(date));
+
   return (
     <div className={styles.overlay} onClick={handleOverlayClick}>
       <div className={styles.modal} ref={modalRef}>
         <button className={styles.closeBtn} onClick={onClose}>
           ×
         </button>
-        <h2>{excursion.title}</h2>
-        <p>{excursion.description}</p>
-        <p>
-          <strong>Location:</strong> {excursion.location}
-        </p>
-        <p>
-          <strong>Price:</strong> {excursion.price} €
-        </p>
-        <p>
-          <strong>Closest date:</strong>{" "}
-          {new Date(excursion.tour_dates?.[0]).toLocaleDateString()}
-        </p>
+        <h2 className={styles.title}>{excursion.title}</h2>
+        <p className={styles.desc}>{excursion.description}</p>
+        <div className={styles.wrapper}>
+          <p className={styles.additionalText}>
+            <strong>Location:&nbsp;</strong> {excursion.location}
+          </p>
+          <p className={styles.additionalText}>
+            <strong>Price:&nbsp;</strong> {excursion.price} €
+          </p>
+        </div>
+
+        <h3 className={styles.additionalText}>Available Dates</h3>
+        <div className={styles.calendarWrapper}>
+          <DayPicker
+            mode="single"
+            selected={undefined}
+            modifiers={{ available: availableDates }}
+            modifiersClassNames={{ available: styles.highlight }}
+          />
+        </div>
       </div>
     </div>
   );
