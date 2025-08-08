be3544977c64a56065719c231f3aafc34adfeea2
ingrida <ingridakvetkovskyte@gmail.com>
2025-08-08 15:04:16 +0300

    Add dark mode

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -4,17 +4,32 @@ import ModalController from "../../pages/Login/ModalController";
 import { Link } from "react-router";
 import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
+import "../../styles/global.css";
 
 const Nav = () => {
   const [showAuth, setShowAuth] = useState(false);
   const [showLogin, setShowLogin] = useState(true);
   const [showRegister, setShowRegister] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
 
   useEffect(() => {
     const saved = localStorage.getItem("theme") || "light";
     document.documentElement.setAttribute("data-theme", saved);
   }, []);
+
+  /*useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);*/
 
   const toggleTheme = () => {
     console.log("theme toggled");
@@ -63,7 +78,7 @@ const Nav = () => {
   };
 
   return isLoggedIn ? (
-    <nav className="w-full flex-row flex h-[8rem] bg-white sticky shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-8 max-md:px-4 max-lg:px-12 top-0">
+    <nav className="w-full flex-row flex h-[8rem] sticky bg-[var(--lighter-background-color)] shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-8 max-md:px-4 max-lg:px-12 top-0 ">
       <div className="flex w-3/4  max-2xl:w-1/2 max-md:w-1/3">
         <Link to="/">
           <h2 className="font-['Nunito_Sans',sans-serif] font-extrabold text-[2.4rem] max-md:text-[1.4rem]  ">
@@ -104,7 +119,7 @@ const Nav = () => {
       )}
     </nav>
   ) : (
-    <nav className="w-full flex-row flex h-[8rem] bg-white sticky shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-8 max-md:px-4 max-lg:px-12 top-0">
+    <nav className="w-full flex-row flex h-[8rem] sticky bg-[var(--lighter-background-color)] shadow-[0rem_0.125rem_0.25rem_0rem_rgba(0,0,0,0.0562)] items-center px-8 max-md:px-4 max-lg:px-12 top-0">
       <div className="flex w-3/4  max-2xl:w-1/2">
         <Link to="/">
           <h2 className="font-['Nunito_Sans',sans-serif] font-extrabold text-[2.4rem] max-md:text-[1.4rem]  ">
@@ -135,7 +150,10 @@ const Nav = () => {
           <img src={moon} alt="Dark mode icon" className="max-md:hidden" />
           <button
             className="font-['Nunito_Sans',sans-serif] font-semibold text-[1.6rem] max-md:text-[1.2rem] cursor-pointer"
-            onClick={toggleTheme}
+            onClick={() => {
+              toggleTheme();
+              setDarkMode(!darkMode);
+            }}
           >
             Dark Mode
           </button>
