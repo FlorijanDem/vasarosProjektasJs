35c24eb7a0dcac698748f9fb02f40da875bcae3f
[Eidenas] <[eidenaspunys@gmail.com]>
2025-08-11 13:27:10 +0300

    Add base of Admin Panel page

--- frontend/src/components/ExcursionCard/ExcursionCardAdmin.jsx
+++ frontend/src/components/ExcursionCard/ExcursionCardAdmin.jsx
@@ -1,54 +1,22 @@
-import { useState } from "react";
-import { ClipLoader } from "react-spinners";
 import styles from "./excursionCard.module.css";
-import { renderStars } from "../../utils/renderStars";
-import {
-  getClosestDate,
-  formatInterval,
-} from "../../utils/dateTimeManipulations";
-import DefaultExcursionImg from "../../assets/default-tour-img.avif";
 
 const ExcursionCard = ({ excursion }) => {
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
-
   return (
     <article className={styles.card}>
       <figure className={styles.figure}>
-        {!isImageLoaded && (
-          <div className={styles.loader}>
-            <ClipLoader color="#808080" size={20} />
-          </div>
-        )}
-        <img
-          className={`${styles.cardImg} ${!isImageLoaded ? styles.hidden : ""}`}
-          src={excursion.photo_url || DefaultExcursionImg}
-          alt={excursion.title}
-          onLoad={() => setIsImageLoaded(true)}
-          onError={(e) => {
-            e.target.onerror = null;
-            e.target.src = DefaultExcursionImg;
-          }}
-        />
         <div className={styles.textContainer}>
           <h2 className={styles.cardTitle}>{excursion.title}</h2>
-          <div className={styles.ratingSection}>
-            <div className={styles.stars}>
-              {renderStars(excursion.average_rating)}
-            </div>
-            <p className={styles.rating}>{excursion.average_rating}</p>
-          </div>
-          <p className={styles.dateSection}>{excursion.location}</p>
-          <div className={styles.dateSection}>
-            <p>Closest date: </p>
-            <p>{getClosestDate(excursion.tour_dates)?.toLocaleDateString()}</p>
-          </div>
-          <div className={styles.durationPriceSection}>
-            <p>{formatInterval(excursion.duration)}</p>
-            <p>{`${excursion.price} €`}</p>
-          </div>
-          <div className={styles.btnContainer}>
-            <button className={styles.moreInfoBtn}>More info</button>
-          </div>
+        </div>
+        <div className="w-full h-full flex flex-col px-10 text-xl">
+          <h2>Participating: </h2>
+          <h2>Canceled: </h2>
+        </div>
+        <div className="flex w-full justify-end p-8">
+          <button
+            className=" rounded-lg border-2 text-2xl text-gray-400 cursor-pointer inline-block font-medium leading-5 m-0 px-3 py-2 text-center transition-all duration-200"
+          >
+            Modify
+          </button>
         </div>
       </figure>
     </article>
